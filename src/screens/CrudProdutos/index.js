b34612f46915bd96951799f3ff9a734743eca9a2
Lucfc <lucasfcarvalho.17@gmail.com>
2021-07-08 16:14:24 -0300

    imagem icon funcionando

--- src/screens/CrudProdutos/index.js
+++ src/screens/CrudProdutos/index.js
@@ -1,54 +1,58 @@
-import React,{ useEffect, useState} from 'react';
-import { Text, View, TextInput, Image, ScrollView, FlatList } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, TextInput, Image, ScrollView, FlatList} from 'react-native';
 
-import { styles } from './styles';
+import {styles} from './styles';
 import ProdutoService from './produtoService';
 
-import backIcon from '../../assets/icons/res/drawable-hdpi/outline_arrow_back_black_18.png';
+import IconBack from '../../assets/icons/backArrow.png';
 
 export const CrudProdutos = () => {
+  const [produtos, setProdutos] = useState([]);
+  const produtoService = new ProdutoService();
 
-    const [produtos, setProdutos] = useState([]);
-    const produtoService = new ProdutoService();
+  useEffect(() => {
+    produtoService.getProdutos().then(data => setProdutos(data));
+  }, []);
 
-    useEffect(() => {
-        produtoService.getProdutos().then((data) => setProdutos(data));
-      }, []);
-
-    return(
-        <View>
+  return (
+    <View>
+      <View>
+        <Image source={IconBack} />
+        <Text>Login</Text>
+      </View>
+      <View>
+        <Text>Novo produto</Text>
+        {/* <Image/> */}
+      </View>
+      <View>
+        <TextInput style={styles.inputSearch}></TextInput>
+      </View>
+      <FlatList
+        data={produtos}
+        renderItem={({item}) => (
+          <ScrollView>
             <View>
-                <Image source={require('../../assets/icons/res/drawable-hdpi/outline_arrow_back_black_18.png')} />
-                <Text>Login</Text>
+              <Text>{item.id}</Text>
+              <Text>{item.nome}</Text>
             </View>
             <View>
-                <Text>Novo produto</Text>
-                {/* <Image/> */}
-            </View>
-            <View>
-                <TextInput style={styles.inputSearch}></TextInput>
-            </View>
-            <FlatList data={produtos} renderItem={ ({ item }) =>
-                <ScrollView>
-                    <View>
-                        <Text>{item.id}</Text>
-                        <Text>{item.nome}</Text> 
-                    </View>
-                    <View>
-                        <Image style={{width:50, height:50}} source={{uri:item.fotoLink}} />
-                        <View>
-                            <Text>{item.descricao}</Text>
-                            <Text>{item.nomeCategoria}</Text>
-                            <Text>{item.valor}</Text>   
-                        </View>
-                        <View>
-                            {/* <Image source={caneta}></Image>
+              <Image
+                style={{width: 50, height: 50}}
+                source={{uri: item.fotoLink}}
+              />
+              <View>
+                <Text>{item.descricao}</Text>
+                <Text>{item.nomeCategoria}</Text>
+                <Text>{item.valor}</Text>
+              </View>
+              <View>
+                {/* <Image source={caneta}></Image>
                             <Image source={lixeira}></Image> */}
-                        </View>
-                    </View>
-                </ScrollView>
-                } keyExtractor={ item => item.id }>
-            </FlatList>
-        </View>
-    )
-}
+              </View>
+            </View>
+          </ScrollView>
+        )}
+        keyExtractor={item => item.id}></FlatList>
+    </View>
+  );
+};
